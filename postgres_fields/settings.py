eb0caa49c28d4fc09b50a81fc3b2749fbc4fd1ea
Razzi Abuissa <razzi53@gmail.com>
2015-11-04 12:22:03 -0800

    Add tickets example

--- postgres_fields/settings.py
+++ postgres_fields/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = (
 
     'django_extensions',
 
-    'customers'
+    'customers',
+    'tickets'
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ tickets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Ticket(models.Model):
+    description = models.TextField()
+    assigned_users = ArrayField(models.IntegerField)
